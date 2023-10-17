c124cf98d5d63d8ebc856f5c0a5deaecdd39bea4
ChenTaiEn <3B032112@gm.student.ncut.edu.tw>
2023-10-18 07:48:10 +0800

    將 Route 的內容搬至 ControIIer 內

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\HomeController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -39,3 +39,4 @@ Route::group(['prefix' => 'admin'], function () {
         return 'admin dashboard';
     });
 });
+Route::get('home', [HomeController::class, 'index'])->name('home.index');
